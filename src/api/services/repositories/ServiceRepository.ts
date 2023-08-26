05dbf913ccb23303b7a2ed5b304827a454155b48
iCarlosAugusto <augusto.dev@proton.me>
2023-08-26 16:18:57 -0300

    Aplicando saveToBucket no serviceReposittory

--- src/api/services/repositories/ServiceRepository.ts
+++ src/api/services/repositories/ServiceRepository.ts
@@ -3,14 +3,22 @@ import { PrismaService } from 'src/api/users/services/prima.service';
 import { CreateServiceDto } from '../dto/create-service.dto';
 import { FindAllServicesDto } from '../dto/find-all-services.dto';
 import { FindByPartnerIdDto } from '../dto/find-by-partnerId.dto';
+import { saveToBucket } from 'src/utils/saveToBucket';
 
 @Injectable()
 class ServiceRepository {
 
   constructor(private prisma: PrismaService) {};
 
-  async create(data: CreateServiceDto) {
-    const service = await this.prisma.service.create({ data });
+  async create(data: CreateServiceDto, file: Express.Multer.File) {
+
+    const url = await saveToBucket(file);
+    const service = await this.prisma.service.create({ 
+      data: {
+        ...data,
+        bannerImage: url 
+      }
+     });
     return service;
   }
   
