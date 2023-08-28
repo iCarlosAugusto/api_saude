c57d1dbf1dbc2f10e0e73c793220ed63eb242b61
iCarlosAugusto <augusto.dev@proton.me>
2023-08-27 21:35:50 -0300

    refactor service

--- src/api/news/news.service.ts
+++ src/api/news/news.service.ts
@@ -7,13 +7,13 @@ export class NewsService {
 
   constructor(private newsRepository: NewsRepository){}
 
-  async create(data: CreateNewsDto) {
-    const news = await this.newsRepository.create(data); 
+  async create(data: CreateNewsDto, file: Express.Multer.File,) {
+    const news = await this.newsRepository.create(data, file); 
     return news;
   }
 
   async findAll() {
     const news = await this.newsRepository.findAll();
-    return news[0];
+    return news;
   }
 }
