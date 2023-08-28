03bf43eb63615d0a0b04c13b38a1f01e50ebf89a
iCarlosAugusto <augusto.dev@proton.me>
2023-08-27 21:35:02 -0300

    accept file in controller

--- src/api/news/news.controller.ts
+++ src/api/news/news.controller.ts
@@ -1,19 +1,30 @@
 import { Resolver, Mutation, Args, Query } from '@nestjs/graphql';
 import { NewsEntity } from './entities/news.entity';
 import { NewsService } from './news.service';
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { CreateNewsDto } from './dtos/create-news.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('/news')
 export class NewsController {
   constructor(private readonly newsService: NewsService) {}
 
+  @UseInterceptors(FileInterceptor('file'))
   @Post('/create')
-  createNews(@Body() createNewsDto: CreateNewsDto) {
-    return this.newsService.create(createNewsDto);
+  createNews(
+    @Body() createNewsDto: CreateNewsDto,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    if(!file) {
+      throw new HttpException(
+        'Faltando o arquivo de imagem',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    return this.newsService.create(createNewsDto, file);
   }
 
-  @Get('/findAll')
+  @Post('/findAll')
   async findNews() {
     return await this.newsService.findAll();
   }
