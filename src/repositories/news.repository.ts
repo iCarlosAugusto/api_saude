b90cb4c42a4c81d76f03a52f67db6e904f7be298
iCarlosAugusto <augusto.dev@proton.me>
2023-08-27 21:35:23 -0300

    refactor news repo

--- src/repositories/news.repository.ts
+++ src/repositories/news.repository.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateNewsDto } from 'src/api/news/dtos/create-news.dto';
 import { PrismaService } from 'src/api/users/services/prima.service';
+import { saveToBucket } from 'src/utils/saveToBucket';
 
 @Injectable()
 export class NewsRepository {
   constructor(private prisma: PrismaService) {}
 
-  async create({ title, description, imageUrl }: CreateNewsDto){
+  async create({ title, description }: CreateNewsDto, file: Express.Multer.File,){
+    const url = await saveToBucket(file);
     return await this.prisma.news.create({
       data: {
         title,
         description,
-        imageUrl
+        imageUrl: url
       }
     });
   }
