f48376028976c38ceafc7d84b4f4839df71eca0e
iCarlosAugusto <augusto.dev@proton.me>
2023-09-04 16:46:21 -0300

    refact service

--- src/api/news/news.service.ts
+++ src/api/news/news.service.ts
@@ -1,19 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { NewsRepository } from 'src/repositories/news.repository';
 import { CreateNewsDto } from './dtos/create-news.dto';
+import { DeleteNewsDto } from './dtos/delete-news.dto';
 
 @Injectable()
 export class NewsService {
 
   constructor(private newsRepository: NewsRepository){}
 
-  async create(data: CreateNewsDto, file: Express.Multer.File,) {
-    const news = await this.newsRepository.create(data, file); 
+  async create(data: CreateNewsDto) {
+    const news = await this.newsRepository.create(data); 
     return news;
   }
 
   async findAll() {
     const news = await this.newsRepository.findAll();
     return news;
+  }
+  
+  async delete(data: DeleteNewsDto) {
+    const news = await this.newsRepository.delete(data);
   }
 }
